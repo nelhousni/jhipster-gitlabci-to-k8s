05fffc96d0130492af18d16a7627beff91aec322
Nordine EL HOUSNI <nordine@nortech.dev>
2020-03-17 15:31:16 +0100

    review helm structure

--- helm-apply.sh
+++ helm-apply.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 # Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
 # Usage: bash helm-apply.sh
-kubectl apply -f namespace.yaml
 helmVersion=$(helm version --client | grep -E "v3\\.[0-9]{1,3}\\.[0-9]{1,3}" | wc -l)
-if [ -d "csvc" ]; then
-helm delete --purge csvc 2>/dev/null
-helm dep up ./csvc
+if [ -d "monitoring" ]; then
+helm delete --purge monitoring 2>/dev/null
+helm dep up ./helm/monitoring
   if [ $helmVersion -eq 1 ]; then
-helm install csvc ./csvc --namespace application
+helm install monitoring ./helm/monitoring --namespace gitlab-managed-apps
   else
-helm install --name csvc ./csvc --namespace application
+helm install --name monitoring ./helm/monitoring --namespace gitlab-managed-apps
   fi
 fi
 helm delete --purge jhipster 2>/dev/null
 helm dep up ./jhipster
 if [ $helmVersion -eq 1 ]; then
-helm install jhipster  ./jhipster --namespace application
+helm install jhipster  ./helm/jhipster --namespace gitlab-managed-apps
 else
-helm install --name jhipster  ./jhipster --namespace application
+helm install --name jhipster  ./helm/jhipster --namespace gitlab-managed-apps
 fi
 
 
--- helm-upgrade.sh
+++ helm-upgrade.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
 # Usage: bash helm-apply.sh
-if [ -d "csvc" ]; then
-helm dep up ./csvc
-helm upgrade --install csvc ./csvc --namespace application
+if [ -d "monitoring" ]; then
+helm dep up ./helm/monitoring
+helm upgrade --install monitoring ./helm/monitoring --namespace gitlab-managed-apps
 fi
-helm dep up ./jhipster
-helm upgrade --install jhipster ./jhipster --namespace application
+helm dep up ./helm/jhipster
+helm upgrade --install jhipster ./helm/jhipster --namespace gitlab-managed-apps
 
 
